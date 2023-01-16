4d0dc2354177e65cdfe4692b4611a61845a1e445
Shelena <elena.shekhova@gmail.com>
2023-01-16 18:13:15 +0300

    3 program added

---
+++ dz1_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+string chislo = Console.ReadLine();
+int a = int.Parse(chislo);
+int b = a % 2;
+if (b == 0)
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+    Console.WriteLine("число нечетное");
+}
