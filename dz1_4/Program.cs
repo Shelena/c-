1601129727c5fc3a241a1fad817f522e2b9197e9
Shelena <elena.shekhova@gmail.com>
2023-01-16 19:16:25 +0300

    4 program added

---
+++ dz1_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+string chislo = Console.ReadLine();
+int a = int.Parse(chislo);
+int x = 1;
+int y = 1;
+for (int i = 0; i < a; i++)
+{
+    x = a - i;
+    y = x % 2;
+    if (y == 0)
+    {
+        Console.Write(x);
+        Console.Write(",");
+    }
+}
