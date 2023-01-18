7dd14eed6c01b3b4b0161ff8d394e2d7a5ed793f
Shelena <elena.shekhova@gmail.com>
2023-01-18 23:34:22 +0300

    3 program added

---
+++ dz2_3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число дня недели");
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case 1:
+        Console.WriteLine("Понедельник не выходной");
+        break;
+    case 2:
+        Console.WriteLine("Вторник не выходной");
+        break;
+    case 3:
+        Console.WriteLine("Среда не выходной");
+        break;
+    case 4:
+        Console.WriteLine("Четверг не выходной");
+        break;
+    case 5:
+        Console.WriteLine("Пятница не выходной");
+        break;
+    case 6:
+        Console.WriteLine("Суббота выходной");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье выходной");
+        break;
+}
