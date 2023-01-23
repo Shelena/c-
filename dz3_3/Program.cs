7e36e4d3ce55a89bd8962398104ebbc6392b3fc8
Shelena <elena.shekhova@gmail.com>
2023-01-23 16:55:11 +0300

    3 program added

---
+++ dz3_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int[] myArray = new int[n];
+for (int i = 0; i < n; i++)
+{
+    int rez = i + 1;
+    rez = rez * rez * rez;
+    myArray[i] = rez;
+}
+Console.WriteLine("Таблица кубов:");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(myArray[i]);
+    Console.Write(", ");
+}
