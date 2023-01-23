6f37e58be82a546c86cc2ea718da5c3d6b2b4349
Shelena <elena.shekhova@gmail.com>
2023-01-23 15:35:43 +0300

    1 program added

---
+++ dz3_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] myArray = new int[5];
+for (int i = 0; i < 5; i++)
+{
+
+    int num = a % 10;
+    a /= 10;
+    myArray[i] = num;
+}
+if (myArray[0] == myArray[4] & myArray[1] == myArray[3])
+{
+    Console.WriteLine("это палиндром");
+}
+else Console.WriteLine("это не палиндром");
