3e4daed9c9758384da132896347eb568f0ac752c
Shelena <elena.shekhova@gmail.com>
2023-01-25 17:30:14 +0300

    1 program added

---
+++ dz4_1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите степень");
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine("Введите число");
+int x = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+//double result = Math.Pow(x, n);
+//Console.WriteLine(result);
+string binary = Convert.ToString(n, 2);
+string kx_str = binary.Replace("0", "k");
+kx_str = kx_str.Replace("1", "kx");
+kx_str = kx_str.Substring(2);
+int res = x;
+foreach (char с in kx_str)
+{
+    if (с == 'k')
+    {
+        res *= res;
+    }
+    else res *= x;
+}
+Console.WriteLine(res);
