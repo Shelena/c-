836a48126a214caa6e555294f16704ad0daf8344
Shelena <elena.shekhova@gmail.com>
2023-01-26 18:17:14 +0300

    2 program added

---
+++ dz4_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int rezult = 0;
+for (int i = 0; n > 0; i++)
+{
+    rezult = rezult + n % 10;
+    n = n / 10;
+}
+Console.Write("Сумма цифр:");
+Console.WriteLine(rezult);
