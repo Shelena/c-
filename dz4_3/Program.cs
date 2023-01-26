ffa8253cb45d597df21d86aac32cab9c4f6dd2b0
Shelena <elena.shekhova@gmail.com>
2023-01-26 18:33:23 +0300

    3 program added

---
+++ dz4_3/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] myArray = new int[8];
+Console.WriteLine("Введите 8 чисел");
+for (int i = 0; i < 8; i++)
+{
+    int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    myArray[i] = n;
+}
+Console.Write("Получился массив: [");
+for (int i = 0; i < 8; i++)
+{
+    Console.Write(myArray[i]);
+}
+Console.WriteLine("]");
