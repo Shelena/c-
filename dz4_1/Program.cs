12762fbc5582e6a5db1223c1777b8f2f7029b7c9
Shelena <elena.shekhova@gmail.com>
2023-01-31 13:58:57 +0300

    1 program added

--- dz4_1/Program.cs
+++ dz4_1/Program.cs
@@ -4,12 +4,12 @@ Console.WriteLine("Введите число");
 int x = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 //double result = Math.Pow(x, n);
 //Console.WriteLine(result);
-string binary = Convert.ToString(n, 2);
-string kx_str = binary.Replace("0", "k");
-kx_str = kx_str.Replace("1", "kx");
-kx_str = kx_str.Substring(2);
+string binary = Convert.ToString(n, 2); // перевод в двоичный код 
+string kx_str = binary.Replace("0", "k"); // замена 0 на k
+kx_str = kx_str.Replace("1", "kx"); // замена 1 на kx
+kx_str = kx_str.Substring(2); // отсечение первых двух kx
 int res = x;
-foreach (char с in kx_str)
+foreach (char с in kx_str) // перебор символов строки
 {
     if (с == 'k')
     {
---
+++ dz5_1/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] myArray = new int[10];
+Random rnd = new Random();
+int res = 0;
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = rnd.Next(0, 999);
+    Console.Write(myArray[i] + " ,");
+    int buf = myArray[i] % 2;
+    if (buf == 0)
+    {
+        res = res + 1;
+    }
+}
+Console.Write(System.Environment.NewLine + "количество четных чисел: " + res + System.Environment.NewLine);
