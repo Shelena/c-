59e93152f7c72049891b391982ac9fc1eb68381e
Shelena <elena.shekhova@gmail.com>
2023-02-08 13:39:52 +0300

    1 program added

---
+++ dz6_1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите числа через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Кол-во чисел больше нуля: {count}");
