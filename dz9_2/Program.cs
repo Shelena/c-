df9658c2c39d7260e03efdd3584eafefb034eec6
Shelena <elena.shekhova@gmail.com>
2023-02-19 15:32:37 +0300

    2 program added

---
+++ dz9_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+
+void GetSum(int m, int n, int res)
+{
+    if (m > n)
+    {
+        Console.WriteLine(res);
+        return;
+    }
+    res = res + (m++);
+    GetSum(m, n, res);
+}
+GetSum(m, n, 0);
