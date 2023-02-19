098c31cdcf6f2e6e760f37ffecdd9870184efbbd
Shelena <elena.shekhova@gmail.com>
2023-02-19 14:36:57 +0300

    1 program added

---
+++ dz9_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("*****");
+void NumberCounter(int n)
+{
+    if (n == 0) return;
+    Console.WriteLine(n);
+    NumberCounter(n - 1);
+}
+NumberCounter(num);
